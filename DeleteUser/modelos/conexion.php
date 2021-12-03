cefcdddce314d9056f39ce6c2d09b8daa71218f7
OrlandoOrtiz1079 <72176412+OrlandoOrtiz1079@users.noreply.github.com>
2021-12-03 13:39:11 -0600

    commit

--- DeleteUser/modelos/conexion.php
+++ DeleteUser/modelos/conexion.php
@@ -4,7 +4,7 @@ class Conexion{
 
 	static public function conectar(){
 
-		$link = new PDO("mysql:host=localhost;dbname=cat","root","");
+		$link = new PDO("mysql:host=localhost;dbname=cat","u297166837.dsc","DscIti2021.");
 		// $link = new PDO("mysql:host=localhost;dbname=cat","root","patas");
 		$link->exec("set names utf8");
 		return $link;
--- SDR/modelos/conexion.php
+++ SDR/modelos/conexion.php
@@ -1,13 +1,14 @@
 <?php
 
-class Conexion{
+class Conexion
+{
 
-	static public function conectar(){
+	static public function conectar()
+	{
 
-		$link = new PDO("mysql:host=localhost;dbname=cat","root","");
+		$link = new PDO("mysql:host=localhost;dbname=cat", "u297166837.dsc", "DscIti2021.");
 		// $link = new PDO("mysql:host=localhost;dbname=cat","root","patas");
 		$link->exec("set names utf8");
 		return $link;
 	}
-
 }
