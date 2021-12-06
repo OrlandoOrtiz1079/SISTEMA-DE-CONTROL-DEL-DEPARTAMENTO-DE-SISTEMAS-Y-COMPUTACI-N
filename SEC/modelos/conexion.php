5fad2c8ec09b78d590a39ceecc0cc79bfb0e231b
OrlandoOrtiz1079 <72176412+OrlandoOrtiz1079@users.noreply.github.com>
2021-12-06 12:14:45 -0600

    commit

--- SEC/modelos/conexion.php
+++ SEC/modelos/conexion.php
@@ -6,7 +6,7 @@ class Conexion
 	static public function conectar()
 	{
 
-		$link = new PDO("mysql:host=localhost;dbname=cat", "u297166837.dsc", "DscIti2021.");
+		$link = new PDO("mysql:host=localhost;dbname=cat","u297166837.dsc","DscIti2021.");
 		// $link = new PDO("mysql:host=localhost;dbname=sdr","root","patas");
 		$link->exec("set names utf8");
 		return $link;
