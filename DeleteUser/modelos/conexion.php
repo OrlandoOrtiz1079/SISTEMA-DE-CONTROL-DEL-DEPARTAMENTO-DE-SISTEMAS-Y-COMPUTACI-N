58b1185e3d789745cb5e0718fe59210cc150f53f
OrlandoOrtiz1079 <72176412+OrlandoOrtiz1079@users.noreply.github.com>
2022-01-11 13:33:25 -0600

    .

--- DeleteUser/modelos/conexion.php
+++ DeleteUser/modelos/conexion.php
@@ -3,8 +3,8 @@
 class Conexion{
 
 	static public function conectar(){
-		$link = new PDO("mysql:host=localhost;dbname=cat","root","");
-		// $link = new PDO("mysql:host=localhost;dbname=u297166837_cat","u297166837_dsc","DSCiti2021");
+		// $link = new PDO("mysql:host=localhost;dbname=cat","root","");
+		$link = new PDO("mysql:host=localhost;dbname=u297166837_cat","u297166837_dsc","DSCiti2021");
 		// $link = new PDO("mysql:host=localhost;dbname=cat","root","");
 		$link->exec("set names utf8");
 		return $link;
--- SEC/modelos/conexion.php
+++ SEC/modelos/conexion.php
@@ -5,8 +5,8 @@ class Conexion
 
 	static public function conectar()
 	{
-		$link = new PDO("mysql:host=localhost;dbname=cat","root","");
-		// $link = new PDO("mysql:host=localhost;dbname=u297166837_cat","u297166837_dsc","DSCiti2021");
+		// $link = new PDO("mysql:host=localhost;dbname=cat","root","");
+		$link = new PDO("mysql:host=localhost;dbname=u297166837_cat","u297166837_dsc","DSCiti2021");
 		// $link = new PDO("mysql:host=localhost;dbname=sdr","root","patas");u297166837.dsc
 		$link->exec("set names utf8");
 		return $link;
